4938b79cb16464ee430dad8bb8d1c3b8f965421a
coneonthefloor <briandgls92@gmail.com>
2024-08-27 21:07:07 +0100

    feat: use export

--- src/Comparators.jl
+++ src/Comparators.jl
@@ -2,32 +2,43 @@ module Comparators
 
 using ..TestSuite
 
-macro export_const(e)
-    Base.isexpr(e, :(=), 2) || error("Must be used on an assignment form, got $e")
-    quote
-        export $(e.args[1])
-        const $(e.args[1]) = $(e.args[2])
-    end |> esc
-end
+export to_be,
+to_be_true,
+to_be_false,
+to_be_greater_than,
+to_be_less_than,
+to_be_greater_than_or_equal,
+to_be_less_than_or_equal,
+to_be_in,
+to_be_nothing,
+to_be_typeof,
+to_be_empty,
+to_have_key,
+to_be_subset,
+to_be_setequal,
+to_be_disjoint,
+to_match,
+to_be_valid_email,
+to_throw
 
-@export_const to_be = construct_comparator(===, "to be")
-@export_const to_be_true = Returns(to_be(true))
-@export_const to_be_false = Returns(to_be(false))
-@export_const to_be_greater_than = construct_comparator(>, "to be greater than")
-@export_const to_be_less_than = construct_comparator(<, "to be less than")
-@export_const to_be_greater_than_or_equal = construct_comparator(>=, "to be greater than or equal")
-@export_const to_be_less_than_or_equal = construct_comparator(<=, "to be less than or equal")
-@export_const to_be_in = construct_comparator(in, "to be in")
-@export_const to_be_nothing = construct_comparator(isnothing, "to be nothing")
-@export_const to_be_typeof = construct_comparator(isa, "to be type of")
-@export_const to_be_empty = construct_comparator(isempty, "to be empty")
-@export_const to_have_key = construct_comparator(haskey, "to have key")
-@export_const to_be_subset = construct_comparator(issubset, "to be subset of")
-@export_const to_be_setequal = construct_comparator(issetequal, "to have equal set")
-@export_const to_be_disjoint = construct_comparator(isdisjoint, "to be disjoint of")
-@export_const to_match = construct_comparator((expected, actual) -> occursin(actual, expected), "to match")
-@export_const to_be_valid_email = Returns(to_match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"))
-@export_const to_throw = construct_comparator("to throw") do actual
+to_be = construct_comparator(===, "to be")
+to_be_true = Returns(to_be(true))
+to_be_false = Returns(to_be(false))
+to_be_greater_than = construct_comparator(>, "to be greater than")
+to_be_less_than = construct_comparator(<, "to be less than")
+to_be_greater_than_or_equal = construct_comparator(>=, "to be greater than or equal")
+to_be_less_than_or_equal = construct_comparator(<=, "to be less than or equal")
+to_be_in = construct_comparator(in, "to be in")
+to_be_nothing = construct_comparator(isnothing, "to be nothing")
+to_be_typeof = construct_comparator(isa, "to be type of")
+to_be_empty = construct_comparator(isempty, "to be empty")
+to_have_key = construct_comparator(haskey, "to have key")
+to_be_subset = construct_comparator(issubset, "to be subset of")
+to_be_setequal = construct_comparator(issetequal, "to have equal set")
+to_be_disjoint = construct_comparator(isdisjoint, "to be disjoint of")
+to_match = construct_comparator((expected, actual) -> occursin(actual, expected), "to match")
+to_be_valid_email = Returns(to_match(r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"))
+to_throw = construct_comparator("to throw") do actual
     try
         actual()
         return false
